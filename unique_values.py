6151aa79118ca3e07f379823f8f17e3b2b8a375b
folukeakpobasa <folukeakpobasa@decagon.dev>
2025-04-21 17:08:16 +0100

    feat: unique value

---
+++ unique_values.py
@@ -0,0 +1,8 @@
+# Filter the DataFrame for object columns
+non_numeric = planes.select_dtypes("object")
+print(non_numeric)
+# Loop through columns
+for column in non_numeric.columns:
+  
+  # Print the number of unique values
+  print(f"Number of unique values in {column} column: ", non_numeric[column].nunique())
